2ad0376bcb56e4d24d22c9c4db2dc7121b88c4f8
James White <james@thewhites.co>
2024-02-16 00:10:19 -0600

    Added dependencies, wrapped up markup

--- public/src/main.js
+++
@@ -1,7 +0,0 @@
-import * from '../lib/blob-stream';
-import * from '../lib/fabric';
-import * from '../lib/pdfkit';
-import {
-  Button, Icon, List, Item, Step, Steps, Toolbar, Upload
-} from '../lib/esc';
-
---
+++ src/main.js
@@ -0,0 +1,38 @@
+function upload(event) {
+}
+
+// function download(event) {
+// }
+
+function change(event) {
+}
+
+function draw(event) {
+  // object:added
+  // object:modified
+  // object:removed
+}
+
+function clear(event) {
+}
+
+function undo(event) {
+}
+
+function redo(event) {
+}
+
+function save(event) {
+}
+
+function docx(event) {
+}
+
+function pdf(event) {
+}
+
+function prev(event) {
+}
+
+function next(event) {
+}
